0997d118a6930a18ab124b1ea1abae33459fbc4d
APB98 <borja.basolutions@gmail.com>
2021-05-22 21:21:10 -0500

    changes on the css file and also on the index html to create the new html file

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 // This file runs the application here we should use inquirer to collect information from the users
+// Include packages needed for this application
+// const inquirer = require('inquirer');
+const fs = require('fs');
+const inquirer = require('inquirer');
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const generateHtml = require('./src/generateHtml');
+
+// Create an array of questions for user input
+const questions = [
+
+    {
+        type: 'input',
+        message: 'Hello manager! What is your name?',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'What is your id number?',
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: 'Please type your email adress',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is your office number?',
+        name: 'officeNumber',
+    },
+    {
+        type: 'checkbox',
+        name: 'team',
+        message: 'Would you like to add members to your team?',
+        choices: ['Engineer', 'Intern', 'Employee'],
+    }
+];
+   
+
+//Now write a function if else that determines the choices of the checkbox
+
+
+// TODO: Create a function to write README file
+function writeToFile(data) {
+    fs.writeFile('index.html', generateHtml(data), (err) =>
+        err ? console.log(err) : console.log('Success!')
+    )
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+        .then((data) => {
+            writeToFile(data);
+        });
+
+}
+
+// Function call to initialize app
+init();
--- src/generateHtml.js
+++ src/generateHtml.js
@@ -1,1 +1,73 @@
 // TODO: Create functions to generate markdown for the HTML file 
+//Create a function to generate the markdown for the html file
+function generateHtml(data){
+    const {name, id, email, role, officeNumber, github, school} = data;
+    
+    return `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!--FOUNDATION STYLE API LINK-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/css/foundation.min.css"
+            integrity="sha256-ogmFxjqiTMnZhxCqVmcqTvjfe1Y/ec4WaRj/aQPvn+I=" crossorigin="anonymous">
+        <!--GOOGLE FONTS STYLE LINK-->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet" />
+        <!--Compresed css-->
+        <link rel="stylesheet" href="./dist/style.css">
+    
+        <title>Team Profile Generator</title>
+    </head>
+    
+    <body>
+        <!--First create the header for the markdown-->
+        <header>
+            <h1 class="text-centered">My Team</h1>
+        </header>
+        <!--Then a main section where i am going to deal with the information provided by the users-->
+        <main>
+            <!--First section manager cards-->
+            <section class="card" id="card-manager">
+                <div class="blue">
+                    <h2 class="name">${name}</h2>
+                    <h2 class="work">Manager</h2>
+                </div>
+                <ul>
+                    <li class="table" id="id">ID:${id}</li>
+                    <li class="table" id="mail">Email:${email}</li>
+                    <li class="table" id="office">Office number:${officeNumber}</li>
+                </ul>
+            </section>
+            <!--Second section Engineer card-->
+            <section class="card" id="card-engineer">
+                <div class="blue">
+                    <h2 class="name">${name}</h2>
+                    <h2 class="work">Engineer</h2>
+                </div>
+                <ul>
+                    <li class="table" id="id">ID:${id}</li>
+                    <li class="table" id="mail">Email:${email}</li>
+                    <li class="table" id="github">GitHub:${github}</li>
+                </ul>
+            </section>
+            <!--Third and last section card intern-->
+            <section class="card" id="card-intern">
+                <div class="blue">
+                    <h2 class="name">${name}</h2>
+                    <h2 class="work">Intern</h2>
+                </div>
+                <ul>
+                    <li class="table" id="id">ID:${id}</li>
+                    <li class="table" id="mail">Email:${email}</li>
+                    <li class="table" id="school">School:${school}</li>
+                </ul>
+            </section>
+        </main>
+    </body>
+    
+    </html>`
+}
+
+module.exports = generateHtml;
